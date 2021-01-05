6278f58b6f70967c313cd8907812c92536aafd5f
Pedro Miguez <pedrovieiramiguez@upskill.pt>
2021-01-05 10:40:33 +0000

    Update Empregado.java

--- ExEmpregado/src/main/java/Empregado.java
+++ ExEmpregado/src/main/java/Empregado.java
@@ -1,2 +1,56 @@
 public class Empregado {
+
+    private String primeiroNome;
+    private String ultimoNome;
+    private Data dataContrato;
+    private Tempo horaEntrada;
+    private Tempo horaSaida;
+
+    public Empregado (String primeiroNome, String ultimoNome, Data dataContrato, Tempo horaEntrada, Tempo horaSaida) {
+        setPrimeiroNome(primeiroNome);
+        setUltimoNome(ultimoNome);
+        setDataContrato(new Data (dataContrato));
+        setHoraEntrada(new Tempo (horaEntrada));
+        setHoraSaida(new Tempo(horaSaida));
+    }
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getUltimoNome() {
+        return ultimoNome;
+    }
+
+    public void setUltimoNome(String ultimoNome) {
+        this.ultimoNome = ultimoNome;
+    }
+
+    public Data getDataContrato() {
+        return dataContrato;
+    }
+
+    public void setDataContrato(Data dataContrato) {
+        this.dataContrato = dataContrato;
+    }
+
+    public Tempo getHoraEntrada() {
+        return horaEntrada;
+    }
+
+    public void setHoraEntrada(Tempo horaEntrada) {
+        this.horaEntrada = horaEntrada;
+    }
+
+    public Tempo getHoraSaida() {
+        return horaSaida;
+    }
+
+    public void setHoraSaida(Tempo horaSaida) {
+        this.horaSaida = horaSaida;
+    }
 }
