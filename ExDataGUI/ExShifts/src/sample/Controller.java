b31f415cef103c89f8bf15df3fb9c96a4030e323
Pedro Miguez <pedrovieiramiguez@upskill.pt>
2021-01-12 17:47:38 +0000

    finalização design

--- ExDataGUI/ExShifts/src/sample/Controller.java
+++ ExDataGUI/ExShifts/src/sample/Controller.java
@@ -1,4 +1,23 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
 public class Controller {
+    public TextArea numText;
+    public Button btnConfirm;
+    public Label letrasLabel;
+    public Button btnLeft;
+    public Button btnRight;
+
+    public void btnConfirmClick(ActionEvent actionEvent) {
+    }
+
+    public void btnLeftClick(ActionEvent actionEvent) {
+    }
+
+    public void btnRightClick(ActionEvent actionEvent) {
+    }
 }
